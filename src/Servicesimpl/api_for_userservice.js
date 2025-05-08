ce4cd724b075f0861f7905d59ec5044343366ed8
Himanshu Devi <himanshu2devi@gmail.com>
2025-05-09 02:40:57 +0530

    Update api_for_userservice.js

--- src/Servicesimpl/api_for_userservice.js
+++ src/Servicesimpl/api_for_userservice.js
@@ -1,8 +1,29 @@
 import axios from 'axios';
-//import api_for_userService from './api_for_userservice';
 
 const api_for_userService = axios.create({
-  baseURL: 'http://localhost:8081/api/auth', 
+  baseURL: 'http://localhost:8081/api/auth',
 });
+
+// Add Authorization header to every request if token is present
+api_for_userService.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+// Endpoint to login (no token needed)
+export const loginUser = ({ username, password }) => {
+  return api_for_userService.post('/login', { username, password });
+};
+
+// Authenticated call to fetch user details
+export const getUserByUsername = username => {
+  return api_for_userService.get(`/users/${username}`);
+};
 
 export default api_for_userService;
