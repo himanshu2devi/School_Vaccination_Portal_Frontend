7e088b313b2b8ba3c4daa42922118fdf53ee6009
Himanshu Devi <himanshu2devi@gmail.com>
2025-05-09 02:40:20 +0530

    Update driverServiceImpl.js

--- src/Servicesimpl/driverServiceImpl.js
+++ src/Servicesimpl/driverServiceImpl.js
@@ -1,7 +1,23 @@
 import api_for_driveService from '../Servicesimpl/api_for_driveService';
 
 export const getDrives = () => api_for_driveService.get('/drives');
-export const addDrive = (drive) => api_for_driveService.post('/drives', drive);
-export const deleteDrive = (id) => api_for_driveService.delete(`/drives/${id}`);
-export const updateDrive = (id, drive) => api_for_driveService.put(`/drives/${id}`, drive);
 
+export const addDrive = (drive) => api_for_driveService.post('/drives', drive);
+
+export const deleteDrive = (id) => api_for_driveService.delete(`/drives/${id}`);
+
+export const updateDrive = (id, drive, username) => {
+  return api_for_driveService.put(`/drives/${id}`, drive, {
+    headers: {
+      username: username
+    }
+  });
+};
+
+export const approveDrive = (id, updatedDrive, username) => {
+  return api_for_driveService.put(`/drives/${id}`, updatedDrive, {
+    headers: {
+      username: username
+    }
+  });
+};
